7a96cd5388824ce80e4e3125e728eacc84f45060
Ethan Shook <eshook89@gmail.com>
2020-07-18 09:52:02 -0500

    added generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  return "hello";
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
