3449ac50d9e206afb00f83672bb420caf08c2943
Ethan Shook <eshook89@gmail.com>
2020-07-20 13:53:10 -0500

    added function and value string

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Make function that generates password
 function generatePassword() {
-  return "hello";
+  let completxity = prompt(
+    'How many characters would you like in your password? ("8 - 128'
+  );
+
+  let values =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()/?-_";
 }
 // Write password to the #password input
 function writePassword() {
