7410e5ef3267ed7f2b15cf4880df364bee69e02c
Ethan Shook <eshook89@gmail.com>
2020-07-20 14:10:39 -0500

    fixed typo

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 // Make function that generates password
 function generatePassword() {
-  let completxity = prompt(
+  let complexity = prompt(
     'How many characters would you like in your password? ("8 - 128'
   );
 
   let values =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()/?-_";
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
