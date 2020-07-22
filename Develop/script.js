4cc4467007ff6a9354c032bc9ea597de13ead9eb
Ethan Shook <eshook89@gmail.com>
2020-07-22 15:09:04 -0500

    turned character strings into arrays

--- Develop/script.js
+++ Develop/script.js
@@ -26,15 +26,23 @@ function generatePassword() {
   }
 
   var rpassword = "";
-  var allowed = {};
-  if (upper) password += allowed.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  if (lower) password += allowed.lower = "abcdefghijklmnopqrstuvwxyz";
-  if (numbers) password += allowed.numbers = "1234567890";
-  if (symbols) password += allowed.symbols = "!@#$%^&*()-_=+";
+  var upper = [
+    "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
+  ];
+  var lower = [
+    "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
+  ];
+  var numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
+  var symbols = ["!, @, #, $, %, ^, &, *, (, ), -, _, =, +"];
+  if (upper) rpassword += upper;
+  if (lower) rpassword += lower;
+  if (numbers) rpassword += numbers;
+  if (symbols) rpassword += symbols;
 
   for (var i = 0; i < length; i++) {
-    rpassword = allowed + allowed.charAt(Math.floor(Math.random() * length));
+    rpassword += rpassword.charAt(Math.floor(Math.random() * rpassword.length));
   }
+  console.log(rpassword);
 }
 
 // Write password to the #password input
