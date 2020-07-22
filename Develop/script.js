32f065041e3832710f76b30251991902f7a50418
Ethan Shook <eshook89@gmail.com>
2020-07-21 22:38:19 -0500

    bug fixes: broken code. made prompts and alerts work

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,55 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+document.querySelector("#generate").addEventListener("click", writePassword);
+
 // Make function that generates password
 function generatePassword() {
-  let length = Number(prompt(
-    'How many characters would you like in your password? ("8 - 128")';))
-    if (lengthPrompt < 8 || lengthPrompt > 128 || isNaN(lengthPrompt)) {
-      alert('Please enter number between 8 and 128');
-  var upper = confirm('Use uppercase?');
-  var lower = confirm('Use lowercase?');
-  var numbers = confirm('Use numbers?');
-  var symbols = confirm('Use symbols?');
+  var length = prompt(
+    "How many characters would you like in your password? (8 - 128)"
+  );
+  if (length < 8 || length > 128 || isNaN(length)) {
+    alert("Please enter number between 8 and 128");
+    var length = prompt(
+      "How many characters would you like in your password? (8 - 128)"
+    );
 
-  if(!upper && !lower && !numbers && !symbols) {
-    alert('Please select one or more character types.');
-    var upper = confirm('Use uppercase?');
-    var lower = confirm('Use lowercase?');
-    var numbers = confirm('Use numbers?');
-    var symbols = confirm('Use symbols?');  
+    var upper = confirm("Use uppercase?");
+    var lower = confirm("Use lowercase?");
+    var numbers = confirm("Use numbers?");
+    var symbols = confirm("Use symbols?");
+  }
+  if (!upper && !lower && !numbers && !symbols) {
+    alert("Please select one or more character types.");
+    var upper = confirm("Use uppercase?");
+    var lower = confirm("Use lowercase?");
+    var numbers = confirm("Use numbers?");
+    var symbols = confirm("Use symbols?");
   }
 
-  window.addEventListener('load', function() {
-    generateNewPassword();
-  });
-  
-  function generateNewPassword() {
-    var password = '';
-    var allowed = {};
-    if (upper) password += (allowed.upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-    if (lower) password += (allowed.lower = 'abcdefghijklmnopqrstuvwxyz')
-    if (numbers) password += (allowed.numbers = '1234567890')
-    if (symbols) password += (allowed.symbols = '!@#$%^&*()-_=+')
-  
-  for(var i = 0; i >= length; i++){
-    password = values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)))
+  var password = "";
+  var allowed = {};
+  if (upper) password += allowed.upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lower) password += allowed.lower = "abcdefghijklmnopqrstuvwxyz";
+  if (numbers) password += allowed.numbers = "1234567890";
+  if (symbols) password += allowed.symbols = "!@#$%^&*()-_=+";
+
+  for (var i = 0; i >= length; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * length));
   }
-  }
- // let values =
-  //  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()/?-_';
-
- // let password = ''
-
-
+  return finalPassword;
 }
+// let values =
+//  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()/?-_';
+
+// let password = ''
+// }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
