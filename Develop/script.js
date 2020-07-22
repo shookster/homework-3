5866ce0daf26a9b74c8814a17df9ed48c4932d1b
Ethan Shook <eshook89@gmail.com>
2020-07-22 15:50:12 -0500

    fixed arrays so password generator works

--- Develop/script.js
+++ Develop/script.js
@@ -27,13 +27,63 @@ function generatePassword() {
 
   var passChar = [];
   var upper = [
-    "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
   ];
   var lower = [
-    "a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
   ];
-  var numbers = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"];
-  var symbols = ["!, @, #, $, %, ^, &, *, (, ), -, _, =, +"];
+  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "?"];
   if (confirmSymbols) {
     passChar = passChar.concat(upper);
   }
@@ -47,8 +97,10 @@ function generatePassword() {
     passChar = passChar.concat(symbols);
   }
 
+  var rPassword = "";
   for (var i = 0; i < length; i++) {
-    rPassword += passChar[Math.floor(Math.random() * passChar.length)];
+    rPassword =
+      rPassword + passChar[Math.floor(Math.random() * passChar.length)];
   }
   return rPassword;
 }
